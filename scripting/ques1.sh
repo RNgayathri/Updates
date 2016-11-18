7c7f8dd557cdcf7ee540c81c5862621187a99c90
RNgayathri <gayathri.rn@razorthink.net>
2016-11-18 15:13:50 +0530

    commit finish

--- scripting/ques1.sh
+++ scripting/ques1.sh
@@ -3,4 +3,4 @@ clear
 mkdir timestamp
 cd /tmp
 mv test /home/gayathri/timestamp
-echo created folder timestamp and moved the test file in /tmp to created directory if the file exist
+echo "created folder timestamp and moved the test file in /tmp to created directory if the file exist"
