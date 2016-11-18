d3d32d83d4f8694937c5abc23ce4ed37bfa9f628
RNgayathri <gayathri.rn@razorthink.net>
2016-11-18 14:19:27 +0530

    commit with question3

--- scripting/ques2.sh
+++ scripting/ques2.sh
@@ -1,11 +1,11 @@
-#!bash/bin/
+#!bin/sh
 args=("$@")
 echo File name= ${args[0]} Path= ${args[1]} 
 cd ${args[1]}
 if [ -f ${args[0]} ]; then  
-	echo "file exist and removed the file"
+	echo "File exist and removed the file"
 	rm ${args[0]}
 		else 
- 	echo "doesnot exist and archived the given directory"
-	tar -cf archived2.tar ${args[2]}
+ 	        echo "Doesn't exist and archived the given directory"
+	        tar -cf archived2.tar ${args[2]}
 fi
---
+++ scripting/ques3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ps -o pid >test1.txt
+echo "PID's of running processes are stored in test1.txt"
+
+
