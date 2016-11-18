7d88b41b8f47dce522ead45ed820554c49196dbe
RNgayathri <gayathri.rn@razorthink.net>
2016-11-18 15:48:29 +0530

    commit finish

--- scripting/ques3.sh
+++ scripting/ques3.sh
@@ -2,6 +2,6 @@
 echo "enter the name of the file to store PID's of running processes-"
 read name
 ps -o pid > $name
-echo "PID's of running processes are stored in the file $name"
+echo "PID's of running processes are stored in the file called $name"
 
 
