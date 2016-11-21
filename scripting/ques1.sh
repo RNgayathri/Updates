087b80435c65433b6dc50e1b9d546d67522ce067
RNgayathri <gayathri.rn@razorthink.net>
2016-11-21 13:30:30 +0530

    commit with question4

--- scripting/ques1.sh
+++ scripting/ques1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 clear
-mkdir timestamp
+mkdir $(date)
 cd /tmp
 mv test /home/gayathri/timestamp
 echo "created folder timestamp and moved the test file in /tmp to created directory if the file exist"
